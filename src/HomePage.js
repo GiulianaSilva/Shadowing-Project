8275a7df6fb4b513cc9b045b3fd1d7402cdb06b6
GiulianaSilva <Giulianasilva.e@gmail.com>
2025-09-29 14:09:10 -0400

    styling Search page

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,11 +3,63 @@ import Input from "./components/ui/input";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-         <Input type="email" placeholder="Email" />
+    <div
+    style={{
+            backgroundImage: 'linear-gradient(to right, #f49494, #ededed, #95baff)',
+            padding: '50px',
+            minHeight: '50vh', 
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '20px',
+            
+          }} className="homepage-container"
+    >      
+      <h1
+        style={{ 
+          fontSize: '3rem', 
+          marginBottom: '20px', 
+          color: '#545454' 
+        }}
+        >
+        Find Your Representatives</h1>
+
+    <div
+      style={{
+      display: 'flex', 
+      alignItems: 'center', 
+      gap: '10px', 
+    }}
+    >  
+    <Input
+      type="email"
+      placeholder="Enter your full address"
+      style={{
+        width: '400px',
+        padding: '10px',
+        fontSize: '1rem',
+        borderRadius: '20px',
+        border: '3px solid #ccc',
+        borderColor: '#545454'
+        }}
+      />
+      <button
+        style={{
+        padding: '10px 20px',
+        fontSize: '1rem',
+        borderRadius: '20px',
+        border: 'none',
+        backgroundColor: '#545454',
+        color: '#fff',
+        cursor: 'pointer',
+  }}
+  >
+  Search
+  </button>
 
     </div>
+  </div>
   );
 };
 
--- src/components/ui/input.js
+++ src/components/ui/input.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Input = ({ type, placeholder }) => {
-  return <input type={type} placeholder={placeholder} className="border p-2 rounded" />;
+const Input = ({ type, placeholder, style }) => {
+  return <input type={type} placeholder={placeholder} style={style} />;
 };
 
 export default Input;
+
